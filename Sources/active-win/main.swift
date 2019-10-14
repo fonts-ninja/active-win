9800846a4e2b2030cbc482394924007a1ba631d9
Fonts Ninja <51237453+fonts-ninja@users.noreply.github.com>
2019-10-14 12:01:18 +0200

    Update main.swift

--- Sources/active-win/main.swift
+++ Sources/active-win/main.swift
@@ -10,8 +10,9 @@ let windows = CGWindowListCopyWindowInfo([.optionOnScreenOnly, .excludeDesktopEl
 
 for window in windows {
 	let windowOwnerPID = window[kCGWindowOwnerPID as String] as! Int
+	let windowOwnerName = window[kCGWindowOwnerName as String] as? String ?? ""
 
-	if windowOwnerPID != frontmostAppPID {
+	if (windowOwnerPID != frontmostAppPID && !windowOwnerName.hasPrefix("com.apple.appkit.xpc")) {
 		continue
 	}
 
